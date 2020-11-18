2be919be078f25ee97ca4e11d416f52ffa3b8c08
Expensure <jaspervanloon@outlook.com>
2020-11-18 16:56:49 +0100

    States verbeterd 2/2

--- FSM_Auto.py
+++ FSM_Auto.py
@@ -25,8 +25,8 @@ def poort_state_transitions(txt):
 def opweg_state_transitions(txt):
     splitted_txt = txt.split(None,1)
     situatie, txt = splitted_txt if len(splitted_txt) > 1 else (txt,"")
-    if situatie == "not":
-        newState = "not_state"
+    if situatie == "parkeerplaats gevonden":
+        newState = "geparkeerd"
     elif situatie in opstopredenen:
         newState = "Op weg"
     elif situatie == "uit garage":
@@ -44,9 +44,14 @@ def geparkeerd_state_transitions(txt):
         newState = "error_state"
     return (newState, txt)
 
-def neg_state(txt):
-    print("Hallo")
-    return ("neg_state", "")
+def slagboom_state_transitions(txt):
+    splitted_txt = txt.split(None,1)
+    situatie, txt = splitted_txt if len(splitted_txt) > 1 else (txt,"")
+    if situatie == "Error":
+        newState = "Op weg"
+    else:
+        newState = "Uit"
+    return (newState, txt)
 
 if __name__== "__main__":
     m = StateMachine()
@@ -54,9 +59,8 @@ if __name__== "__main__":
     m.add_state("Python_state", python_state_transitions)
     m.add_state("is_state", is_state_transitions)
     m.add_state("not_state", not_state_transitions)
-    m.add_state("neg_state", None, end_state=1)
-    m.add_state("pos_state", None, end_state=1)
-    m.add_state("error_state", None, end_state=1)
+    m.add_state("Uit", None, end_state=1)
+    m.add_state("Error", None, end_state=1)
     m.set_start("Start")
     m.run("Python is great")
     m.run("Python is difficult")
