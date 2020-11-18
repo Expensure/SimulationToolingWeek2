6a2545aecd3ba7c87d202f3e7ad7da6d789c2b2c
Expensure <jaspervanloon@outlook.com>
2020-11-18 17:01:39 +0100

    States verbeterd 2/2

--- FSM_Auto.py
+++ FSM_Auto.py
@@ -26,13 +26,13 @@ def opweg_state_transitions(txt):
     splitted_txt = txt.split(None,1)
     situatie, txt = splitted_txt if len(splitted_txt) > 1 else (txt,"")
     if situatie == "parkeerplaats gevonden":
-        newState = "geparkeerd"
+        newState = "Geparkeerd"
     elif situatie in opstopredenen:
         newState = "Op weg"
     elif situatie == "uit garage":
         newState = "Slagboom"
     else:
-        newState = "error_state"
+        newState = "Error"
     return (newState, txt)
 
 def geparkeerd_state_transitions(txt):
@@ -41,7 +41,7 @@ def geparkeerd_state_transitions(txt):
     if situatie == "rijden":
         newState = "Op weg"
     else:
-        newState = "error_state"
+        newState = "Error"
     return (newState, txt)
 
 def slagboom_state_transitions(txt):
@@ -56,12 +56,10 @@ def slagboom_state_transitions(txt):
 if __name__== "__main__":
     m = StateMachine()
     m.add_state("Start", start_transitions)
-    m.add_state("Python_state", python_state_transitions)
-    m.add_state("is_state", is_state_transitions)
-    m.add_state("not_state", not_state_transitions)
+    m.add_state("Poort", poort_state_transitions)
+    m.add_state("Op weg", opweg_state_transitions)
+    m.add_state("Geparkeerd", geparkeerd_state_transitions)
+    m.add_state("Slagboom", slagboom_state_transitions)
     m.add_state("Uit", None, end_state=1)
     m.add_state("Error", None, end_state=1)
     m.set_start("Start")
-    m.run("Python is great")
-    m.run("Python is difficult")
-    m.run("Perl is ugly")
